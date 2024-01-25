1b99b20b23cac7c191cf34498cc2f23aaafb0317
Joey Zazzi <Jzazzi56@gmail.com>
2024-01-25 12:19:50 -0800

    added file for getting items, and creating the items array

--- api/database/database.php
+++ api/database/database.php
@@ -11,3 +11,22 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " 
     . $mysqli->connect_error;
 }
+
+//fetch user
+$userQuery = "SELECT * FROM user";
+$userResult = $mysqli->query($userQuery);
+
+//fetch items
+$itemQuery = "SELECT * FROM items";
+$itemResult = $mysqli->query($itemQuery);
+
+//fetch cart
+
+//fetch orders
+
+//fetch order items
+
+//fetch user orders
+
+//close connection
+$mysqli->close();
