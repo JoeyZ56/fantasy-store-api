5d45b34a7bc874b46fb2a55f291dc8859190d36b
username <Jzazzi56@gmail.comn>
2024-01-26 12:45:42 -0800

    finished the items array

--- api/items/items.php
+++ api/items/items.php
@@ -7,56 +7,56 @@ $items_Armor = array(
         "name" => "Dark Steel Set",
         "price" => 2999.99,
         "description" => "Dark Steel Set, it is a heavy armor set that provides high defense and is used by knights.",
-        "image" => "https://cdn.ecommercedns.uk/files/9/210639/8/1250638/negroli-set-dark-steel-2.jpg"
+        "image_url" => "https://cdn.ecommercedns.uk/files/9/210639/8/1250638/negroli-set-dark-steel-2.jpg"
     ),
     array(
         "id" => 2,
         "name" => "Bronze Steel Set",
         "price" => 2699.99,
         "description" => "Bronze Steel Set, it is a heavy armor set that provides high defense and is used by Kings Guards.",
-        "image" => "https://cdn.ecommercedns.uk/files/9/210639/0/1250640/negroli-set-bronze.jpg"
+        "image_url" => "https://cdn.ecommercedns.uk/files/9/210639/0/1250640/negroli-set-bronze.jpg"
     ),
     array(
         "id" => 3,
         "name" => "Steel Set",
         "price" => 1899.99,
         "description" => "Basic Steel Set, it is a heavy armor set that provides high defense and is used by Soldiers.",
-        "image" => "http://cdn.ecommercedns.uk/files/9/210639/1/2049691/gothic-armour-set-steel-finish-1a.jpg"
+        "image_url" => "http://cdn.ecommercedns.uk/files/9/210639/1/2049691/gothic-armour-set-steel-finish-1a.jpg"
     ),
     array(
         "id" => 4,
         "name" => "Nordic Leather Set",
         "price" => 3199.99,
         "description" => "Nordic Leather Set, it is a light armor set that provides high defense and is used by Vikings.",
-        "image" => "https://www.blackravenarmoury.com/wp-content/uploads/2018/05/Odinson-Gilded-Full-Set.jpg"
+        "image_url" => "https://www.blackravenarmoury.com/wp-content/uploads/2018/05/Odinson-Gilded-Full-Set.jpg"
     ),
     array(
         "id" => 5,
         "name" => "Leather & Chainmail Set",
         "price" => 2799.99,
         "description" => "Leather & Chainmail Set, it is a light armor set that provides high defense and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/bc/58/27/bc582755f39eac96c2e9a86a1ed14f74.jpg"
+        "image_url" => "https://i.pinimg.com/originals/bc/58/27/bc582755f39eac96c2e9a86a1ed14f74.jpg"
     ),
     array(
         "id" => 6,
         "name" => "Assassin Set",
         "price" => 1499.99,
         "description" => "Assassin Set, it is a light armor set that provides high defense and is used by Assassins.",
-        "image" => "https://i.etsystatic.com/18044352/r/il/74f5a2/3872424707/il_fullxfull.3872424707_6xy1.jpg"
+        "image_url" => "https://i.etsystatic.com/18044352/r/il/74f5a2/3872424707/il_fullxfull.3872424707_6xy1.jpg"
     ),
     array(
         "id" => 7,
         "name" => "Nordic Winter Set",
         "price" => 700,
         "description" => "Nordic Winter Set, it is a Heavy armor set that provides high defense and is used by Vikings.",
-        "image" => "https://i.pinimg.com/originals/4a/33/f6/4a33f6722e8d12cf410df832e9d90a19.jpg"
+        "image_url" => "https://i.pinimg.com/originals/4a/33/f6/4a33f6722e8d12cf410df832e9d90a19.jpg"
     ),
     array(
         "id" => 8,
         "name" => "Female Ranger Set",
         "price" => 2199.99,
         "description" => "Female Ranger Set, it is a light armor set that provides high defense and is used by Rangers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "image_url" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
     ),
 );
 
@@ -67,70 +67,169 @@ $items_Weapons = array(
         "name" => "Dark Steel Sword",
         "price" => 999.99,
         "description" => "Dark Steel Sword, it is a heavy sword that provides high damage and is used by knights.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "image_url" => "https://www.thearmoury.shop/_uploads/img/products/giant/S5815.jpg"
     ),
     array(
         "id" => 10,
         "name" => "Bronze Steel Sword",
         "price" => 899.99,
         "description" => "Bronze Steel Sword, it is a heavy sword that provides high damage and is used by Kings Guards.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "image_url" => "https://cdn.shopify.com/s/files/1/0038/2789/2293/products/402-09_26afa161-991e-4554-825f-924770222711.jpg?v=1564575565"
     ),
     array(
         "id" => 11,
-        "name" => "Steel Sword",
+        "name" => "Steel Dagger",
         "price" => 799.99,
         "description" => "Basic Steel Sword, it is a heavy sword that provides high damage and is used by Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "image_url" => "http://www.darksword-armory.com/wp-content/uploads/2016/05/black-prince-dagger-1811-medieval-dagger.jpg"
     ),
     array(
         "id" => 12,
-        "name" => "Nordic Leather Sword",
-        "price" => 1099.99,
-        "description" => "Nordic Leather Sword, it is a light sword that provides high damage and is used by Vikings.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "name" => "Short Bow",
+        "price" => 499.99,
+        "description" => "Short Bow, it is a mid ranged weapon that provides high damage and is used by Rangers.",
+        "image_url" => "https://i.pinimg.com/originals/6f/63/78/6f6378484c5b6475907f80e83a4d5ebb.jpg"
     ),
     array(
         "id" => 13,
-        "name" => "Leather & Chainmail Sword",
-        "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "name" => "Long Bow",
+        "price" => 699.99,
+        "description" => "Long Bow, it is a far ranged weapon that provides high damage and is used by Rangers.",
+        "image_url" => "https://www.battlemerchant.com/images/product_images/original_images/0612186140_bogen_langbogen_longbow.jpg"
     ),
     array(
         "id" => 14,
-        "name" => "Leather & Chainmail Sword",
+        "name" => "Crossbow",
         "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "description" => "Crossbow, it is a heavy crossbow that provides high damage and is used by Infantry.",
+        "image_url" => "https://cdn.shopify.com/s/files/1/2080/1501/products/14thC_steel_crossbow_1.JPG?v=1518597730"
     ),
     array(
         "id" => 15,
-        "name" => "Leather & Chainmail Sword",
-        "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "name" => "Great Axe",
+        "price" => 1399.99,
+        "description" => "Two Handed Axe, it is a heavy axe that provides high damage and is used by Vikings.",
+        "image_url" => "https://i.pinimg.com/originals/04/69/7b/04697bbfa2daac59ad080e08b098fc2f.jpg"
     ),
     array(
         "id" => 16,
-        "name" => "Leather & Chainmail Sword",
-        "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "name" => "Spear",
+        "price" => 399.99,
+        "description" => "Spear, it is a light spear that provides high damage and is used by Infantry.",
+        "image_url" => "https://media.sketchfab.com/models/0168a9897b6f47a180e7d63dca25085f/thumbnails/427ae67baaa340af97880a3e5690b7a5/f74afb2f33dc45d5970ec130deb95755.jpeg"
     ),
     array(
         "id" => 17,
-        "name" => "Leather & Chainmail Sword",
-        "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
+        "name" => "Mace",
+        "price" => 899.99,
+        "description" => "Mace, it is a heavy mace that provides high damage and is used by Infantry.",
+        "image_url" => "https://img1.cgtrader.com/items/3125412/774280b861/medieval-mace-3d-model-low-poly-obj-fbx-blend.jpg"
     ),
-    array(
-        "id" => 18,
-        "name" => "Leather & Chainmail Sword",
-        "price" => 999.99,
-        "description" => "Leather & Chainmail Sword, it is a light sword that provides high damage and is used by Foot Soldiers.",
-        "image" => "https://i.pinimg.com/originals/6d/6f/36/6d6f36c1847f7c63c542b66ba4f130d9.jpg"
-    ),
+   
 
 );
+//Shields
+$items_Shields = array(
+    array(
+        "id" => 18,
+        "name" => "Bronze lion Shield",
+        "price" => 899.99,
+        "description" => "Bronze lion Shield, it is a heavy shield that provides high defense and is used by Kings Guards.",
+        "image_url" => "https://i.etsystatic.com/24524275/r/il/3c7e44/3094892427/il_fullxfull.3094892427_l9ly.jpg"
+    ),
+    array(
+        "id" => 19,
+        "name" => "Knights Shield",
+        "price" => 799.99,
+        "description" => "Knights Shield, it is a heavy shield that provides high defense and is used by knights.",
+        "image_url" => "https://i.etsystatic.com/18181025/r/il/f92977/3038074447/il_1588xN.3038074447_kz9f.jpg"
+    ),
+    array(
+        "id" => 20,
+        "name" => "Dark Steel Shield",
+        "price" => 499.99,
+        "description" => "Dark Steel Shield, it is a heavy shield that provides high defense and is used by knights.",
+        "image_url" => "https://i.pinimg.com/originals/75/38/7a/75387a1c3f6aa51a86c4c9d6481ba680.jpg"
+    ),
+);
+
+
+//Potions
+$items_Potions = array(
+    array(
+        "id" => 21,
+        "name" => "Health Potion",
+        "price" => 99.99,
+        "description" => "Health Potion, it is a potion that restores health.",
+        "image_url" => "https://db4sgowjqfwig.cloudfront.net/campaigns/157632/assets/775857/potion_of_healing.png?1504791538"
+    ),
+    array(
+        "id" => 22,
+        "name" => "Mana Potion",
+        "price" => 119.99,
+        "description" => "Mana Potion, it is a potion that restores mana.",
+        "image_url" => "https://i.etsystatic.com/26563387/r/il/ab7809/3257014705/il_fullxfull.3257014705_97ys.jpg"
+    ),
+    array(
+        "id" => 23,
+        "name" => "Stamina Potion",
+        "price" => 79.99,
+        "description" => "Health Potion, it is a potion that restores health.",
+        "image_url" => "https://static.turbosquid.com/Preview/001318/313/3M/_Z.jpg"
+    ),
+    array(
+        "id" => 24,
+        "name" => "Love Potion",
+        "price" => 199.99,
+        "description" => "Love Potion, it is a potion that makes anyone instantly fall in love with you.",
+        "image_url" => "https://i.pinimg.com/originals/0f/76/a5/0f76a5265f9152fab9267a06a0e916cc.jpg"
+    ),
+    array(
+        "id" => 25,
+        "name" => "Invisability Potion",
+        "price" => 399.99,
+        "description" => "Invisability Potion, it is a potion that makes you invisible.",
+        "image_url" => "https://i.etsystatic.com/5522366/r/il/b1ac68/2693806365/il_794xN.2693806365_4f7a.jpg"
+    ),
+    array(
+        "id" => 26,
+        "name" => "Luck Potion",
+        "price" => 49.99,
+        "description" => "Luck Potion, it is a potion that makes you lucky.",
+        "image_url" => "https://content.thirtyonewhiskey.com/wp-content/uploads/2019/12/27115529/00100lrPORTRAIT_00100_BURST20191231111006764_COVER-scaled.jpg"
+    ),
+);
+
+// Grimoires
+$items_Spells = array(
+    array(
+        "id" => 27,
+        "name" => "Demonic Grimoire",
+        "price" => 99.99,
+        "description" => "Demonic Grimoire, it is a grimoire that contains demonic spells.",
+        "image_url" => "https://static.turbosquid.com/Preview/2015/03/09__18_17_26/1.png2af64f20-5f72-4fa4-b4cf-2e803b783eeeOriginal.jpg"
+    ),
+    array(
+        "id" => 28,
+        "name" => "Demonic Grimoire",
+        "price" => 99.99,
+        "description" => "Demonic Grimoire, it is a grimoire that contains demonic spells.",
+        "image_url" => "https://static.turbosquid.com/Preview/2015/03/09__18_17_26/1.png2af64f20-5f72-4fa4-b4cf-2e803b783eeeOriginal.jpg"
+    ),
+    array(
+        "id" => 29,
+        "name" => "Shadow Grimoire",
+        "price" => 99.99,
+        "description" => "Shadow Grimoire, it is a grimoire that contains shadow spells.",
+        "image_url" => "https://64.media.tumblr.com/8335dac03c0d3ea8ab90e7d11df54781/tumblr_oy0hfgQGs31qas1mto7_1280.jpg"
+    ),
+    array(
+        "id" => 30,
+        "name" => "Arcane Grimoire",
+        "price" => 99.99,
+        "description" => "Acrcane Grimoire, it is a grimoire that contains arcane spells.",
+        "image_url" => "https://i.pinimg.com/736x/8f/8a/b5/8f8ab5448157f4b337fda8eca56b4bd8.jpg"
+    ),
+);
+
+
