9f880652dd8f0db6118ea1bf31ce9fa07901e6b5
Joey Zazzi <Jzazzi56@gmail.com>
2024-03-26 15:18:29 -0700

    changed headers to * all domains

--- api/items/endpoints/getGrimoires.php
+++ api/items/endpoints/getGrimoires.php
@@ -1,19 +1,7 @@
 <?php
 //CORS
-// Set the specific origin instead of wildcard
-header('Access-Control-Allow-Origin: hhttps://fantasy-e-commerce-store.vercel.app');
-// Allow credentials
-header('Access-Control-Allow-Credentials: true');
-
-// Add headers to handle preflight requests 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // Accept requests from the specified origin with credentials
-    header('Access-Control-Allow-Origin: https://fantasy-e-commerce-store.vercel.app');
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    exit(0); 
-}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
 
 require_once '../data/grimoires.php';
 
