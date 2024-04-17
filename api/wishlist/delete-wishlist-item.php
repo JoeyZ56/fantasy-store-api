696564d6b80e9f8769a201da72e17f9909edd6e2
Joey Zazzi <Jzazzi56@gmail.com>
2024-04-17 14:18:09 -0700

    created new file for delteing items from wishlist

---
+++ api/wishlist/delete-wishlist-item.php
@@ -0,0 +1,8 @@
+<?php
+require_once "../utilities/session_setting.php";
+require_once "../utilities/cors_header.php";
+require_once './wishlist.php';
+
+// Error Reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
